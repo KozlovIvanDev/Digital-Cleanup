facaeaa148f2d56051f116f8497009025ffce44c
Ivan53135 <ikozlov15.ik@gmail.com>
2021-03-27 20:07:38 +0200

    commit

--- dist/script.d573be0b.js
+++ dist/script.d573be0b.js
@@ -248,7 +248,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64074" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64839" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
