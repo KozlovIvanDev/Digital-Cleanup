1abf5cc92d6afc24fb6f258c4c318983e866a55a
Ivan53135 <ikozlov15.ik@gmail.com>
2021-04-17 00:15:35 +0300

    commit

--- js/script.js
+++ js/script.js
@@ -216,6 +216,30 @@ render();
   }
 
 })();
+const countDate = new Date('apr 22, 2021, 00:00:00').getTime();
+function EartDay(){
+  const now = new Date().getTime();
+  gap = countDate - now;
+
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  const d = Math.floor(gap / (day));
+  const h = Math.floor((gap % (day)) / (hour));
+  const m = Math.floor((gap % (hour)) / (minute));
+  const s = Math.floor((gap % (minute)) / (second));
+
+  document.getElementById('day').innerHTML = d;
+  document.getElementById('hour').innerHTML = h;
+  document.getElementById('minute').innerHTML = m;
+  document.getElementById('second').innerHTML = s;
+}
+
+setInterval(function(){
+  EartDay();
+})
 
 const year = new Date().getFullYear();
     
