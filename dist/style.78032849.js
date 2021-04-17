6b19e745b5bd702b42d0368d2133a4d87b13d546
Ivan53135 <ikozlov15.ik@gmail.com>
2021-04-17 14:32:53 +0300

    main commit

--- dist/style.78032849.js
+++ dist/style.78032849.js
@@ -217,7 +217,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64839" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51016" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
